8b44a0247491a13231c8934ad4f02b3739a2c3c7
AM <etrog95@gmail.com>
2020-01-13 19:13:11 +0200

    components pages items router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './comps/pages/login/login.component';
+import { PostsPageComponent } from './comps/pages/posts-page/posts-page.component';
+import { PostComponent } from './comps/items/post/post.component';
+import { CommentComponent } from './comps/items/comment/comment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    PostsPageComponent,
+    PostComponent,
+    CommentComponent
   ],
   imports: [
     BrowserModule,
